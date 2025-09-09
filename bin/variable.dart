60ee72e6023addef3d8657ee559d5bf83abfa41c
Raihan Ali Ripon <ripon365cse@gmail.com>
2025-09-09 23:28:28 +0600

    variable types

---
+++ bin/variable.dart
@@ -0,0 +1,28 @@
+void main() {
+  // String variable
+  String name = "Raihan";
+
+  // int variable
+  int age = 22;
+
+  // double variable
+  double height = 5.8;
+
+  // bool variable
+  bool isStudent = true;
+
+  // var (Dart automatically understands type)
+  var city = "Dhaka";
+
+  // dynamic (can change type later)
+  dynamic anything = 123;
+  anything = "Now I am a string!";
+
+  // Printing values
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+  print("City: $city");
+  print("Dynamic Variable: $anything");
+}
