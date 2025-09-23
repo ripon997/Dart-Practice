520ccb42a3f5f693f6d0d0e01b9a92dc22241a81
Raihan Ali Ripon <72176613+ripon997@users.noreply.github.com>
2025-09-23 10:53:11 +0600

    number data type

---
+++ bin/for_in.dart
@@ -0,0 +1,11 @@
+void main() {
+  Map<String, String> capitals = {
+    "Bangladesh": "Dhaka",
+    "India": "New Delhi",
+    "Nepal": "Kathmandu"
+  };
+
+  for (String entry in capitals.entries) {
+    print(prop);
+  }
+}
---
+++ bin/number_type.dart
@@ -0,0 +1,10 @@
+void main(){
+
+  int number1;
+  double number2;
+  number1 = 100;
+  number2 = 50.5;
+  print("Integer number is : $number1");
+  print("Float number is : $number2");
+
+}
