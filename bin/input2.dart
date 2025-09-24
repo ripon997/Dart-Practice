494d8f790d4702a8e0d0b12ee27a20e4b6eed906
Raihan Ali Ripon <72176613+ripon997@users.noreply.github.com>
2025-09-25 00:03:07 +0600

    input example 2

---
+++ bin/input2.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+// for simple input from a person and slow
+void main(){
+  String name;
+  int age;
+  String location;
+  double salary;
+
+  print("Enter your name: ");
+  name = stdin.readLineSync()!;
+
+  print("Enter your age: ");
+  age= int.parse(stdin.readLineSync()!);
+
+  print("Enter your location: ");
+  location= stdin.readLineSync()!;
+
+  print("Enter your salary : ");
+  salary=double.parse(stdin.readLineSync()!);
+
+
+  print("This entry person name is : $name");
+  print("This person age is : $age");
+  print("This person location is : $location");
+  print("This person salary is : $salary");
+
+
+
+}
