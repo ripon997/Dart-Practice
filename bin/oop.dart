e40c895e94ba7006c9a0e8c7818a7e176447f7b2
Raihan Ali Ripon <72176613+ripon997@users.noreply.github.com>
2025-11-15 23:56:09 +0600

    oop

---
+++ bin/oop.dart
@@ -0,0 +1,13 @@
+
+class Monitor{
+  String companyName = 'LG';
+  String model = 'MK600';
+}
+
+
+void main()
+{
+  Monitor myMonitor = Monitor();
+  print(myMonitor.companyName);
+  print(myMonitor.model);
+}
