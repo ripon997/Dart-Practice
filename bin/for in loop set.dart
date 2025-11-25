78e73dd1a5d65c27f939ad85786077a9f71ae4f4
Raihan Ali Ripon <72176613+ripon997@users.noreply.github.com>
2025-11-26 01:04:20 +0600

    for in loop set

---
+++ bin/for in loop set.dart
@@ -0,0 +1,12 @@
+void main(){
+  var productList=[
+    {'name':'soap','price':100},
+    {'name':'shapmo','price':20},
+    {'name':'biscuit','price':100},
+    {'name':'pen','price':5},
+    {'name':'khata','price':50},
+  ];
+  for(var oneProduct in productList){
+    print(oneProduct);
+  }
+}
