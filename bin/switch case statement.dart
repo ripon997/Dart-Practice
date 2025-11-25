84cd9df2b88a8c6ed663e942cef033d2381767bd
Raihan Ali Ripon <72176613+ripon997@users.noreply.github.com>
2025-11-25 22:07:55 +0600

    switch case

---
+++ bin/switch case statement.dart
@@ -0,0 +1,28 @@
+void main(){
+  int marks=30;
+
+  switch(marks) {
+    case 80:
+      print("A+");
+      break;
+    case 70:
+      print("A");
+      break;
+    case 60:
+      print("A-");
+      break;
+    case 50:
+      print("B");
+      break;
+    case 40:
+      print("C");
+      break;
+    case 33:
+      print("D");
+      break;
+    default:
+      print("result not found");
+      break;
+  }
+
+}
