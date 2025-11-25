ee1a83920dff5ec436b45dbbc165875d2a7c1838
Raihan Ali Ripon <72176613+ripon997@users.noreply.github.com>
2025-11-25 21:16:59 +0600

    map property

---
+++ bin/map property.dart
@@ -0,0 +1,15 @@
+void main(){
+  var information=new Map();
+  information['name']='ripon';
+  information['age']='12';
+  information['cgpa']='3.00';
+  information['location']='Mirpur';
+
+  print(information);
+  print(information.keys);
+  print(information.values);
+  print(information.length);
+  print(information.isEmpty);
+
+
+}
