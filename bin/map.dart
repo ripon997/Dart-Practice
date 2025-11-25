2717a30af359311cd1b91d5939d81ccd07917796
Raihan Ali Ripon <72176613+ripon997@users.noreply.github.com>
2025-11-25 21:07:02 +0600

    map create

---
+++ bin/map.dart
@@ -0,0 +1,9 @@
+void main(){
+
+  Map<String,String>information={
+    'name':'Raihan',
+    'Location':'Dhaka'
+  };
+  information['country']='Bangladesh';//single element add
+  print(information);
+}
--- bin/set.dart
+++ bin/set.dart
@@ -2,5 +2,8 @@ void main(){
 
   var myCitySet= {'Rajshahi','Rangpur','Dinajpur','Khulna','Rajshahi'};
   print(myCitySet);
-
+  var number=<int>{2,4,2,3,4};
+  print(number.elementAt(1));
+  var x= number.elementAt(2);
+  print(x);
 }
