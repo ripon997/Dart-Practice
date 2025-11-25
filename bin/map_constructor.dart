37af7b27f2ed78fb3f5687c34f462ca5d4204071
Raihan Ali Ripon <72176613+ripon997@users.noreply.github.com>
2025-11-25 21:13:30 +0600

    map constructor

---
+++ bin/map_constructor.dart
@@ -0,0 +1,10 @@
+void main(){
+  var information=new Map();
+  information['name']='ripon';
+  information['age']='12';
+  information['cgpa']='3.00';
+  information['location']='Mirpur';
+
+  print(information);
+
+}
