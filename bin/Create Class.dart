a348ed2cf462134c9209cd580eb0182aa4a734bb
Raihan Ali Ripon <72176613+ripon997@users.noreply.github.com>
2025-11-26 22:11:41 +0600

    create class and object

---
+++ bin/Create Class.dart
@@ -0,0 +1,19 @@
+class MyClass{
+  String myName= "Raihan Ali Ripon";
+  var Alphabet= ['A','B','C','D'];
+
+  addTwonumber(int x,int y){
+    print(x+y);
+  }
+  addThreenumber(int x,int y,int z){
+    print(x+y+z);
+  }
+}
+
+
+
+void main(){
+var obj = MyClass();
+obj.addTwonumber(3, 5);
+print(obj.Alphabet);
+}
